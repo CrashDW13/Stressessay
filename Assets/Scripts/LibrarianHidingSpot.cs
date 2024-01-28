94ae3e72682abbe9a5a304bee7147ed943405c39
Noah <dacrashbomber@gmail.com>
2024-01-28 16:07:59 -0500

    push for pull

--- Assets/Scripts/LibrarianHidingSpot.cs
+++ Assets/Scripts/LibrarianHidingSpot.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor.Rendering;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,10 +10,14 @@ public class LibrarianHidingSpot : MonoBehaviour
     private float _direction;
     private bool _shaking;
     private Image _librarian;
+    private bool _scaring = false;
 
+    private float _movementFrame = 0f;
+    private float _movementDuration = 10f;
+    private float _libraryFrame = 0f;
     private float _librarianAppearanceX = 630;
     private float _librarianDisappearanceX = 1350;
-    private float _librarianDuration = 3000f;
+    private float _librarianDuration = 4f;
 
     public IEnumerator Shake(float _duration, bool _jumpscare)
     {
@@ -29,6 +34,21 @@ public class LibrarianHidingSpot : MonoBehaviour
         transform.eulerAngles = _startRotation;
         if (_jumpscare)
         {
+
+        }
+    }
+
+    private void Update()
+    {
+        if (_scaring)
+        {
+            _libraryFrame += Time.deltaTime;
+            if (_movementFrame < _movementDuration)
+            {
+                _movementFrame += Time.deltaTime;
+            }
+
+            _librarian.rectTransform.position = new Vector3(Mathf.Lerp(_librarianDisappearanceX, _librarianAppearanceX, _movementFrame / _movementDuration), _librarian.rectTransform.position.y, _librarian.rectTransform.position.z);
 
         }
     }
